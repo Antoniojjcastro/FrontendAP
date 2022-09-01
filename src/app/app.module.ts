d34e2794ebf6f2e1ae024a0d6da5ba8080a3390b
CarolinaHeredia <herediacarolina@gmail.com>
2022-09-01 09:41:38 -0300

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IniciarSesionComponent } from './Component/iniciar-sesion/iniciar-sesion.component';
+import { PortfolioComponent } from './Component/portfolio/portfolio.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IniciarSesionComponent,
+    PortfolioComponent
   ],
   imports: [
     BrowserModule,
